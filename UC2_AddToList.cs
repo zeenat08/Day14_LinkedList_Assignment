640635ea0f8d77f5585b3b7df84d4b288dafc714
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:52:58 +0530

    ZEENAT: created list and added value to it

---
+++ UC2_AddToList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_LinkedList_Assignments
+{
+    class UC2_AddToList
+    {
+
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+    }
+}
+
