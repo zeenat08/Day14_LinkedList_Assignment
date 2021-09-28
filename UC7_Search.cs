753a95f3e479a13028676ef6bac5d90d7555ac16
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:56:54 +0530

    ZEENAT: added searching a value from list

---
+++ UC7_Search.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_LinkedList_Assignments
+{
+    class UC7_Search
+    {
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        internal bool Search(Node head, int x)
+        {
+            Node current = head; // Initialize current
+            while (current != null)
+            {
+                if (current.data == x)
+                    return true; // data found
+                current = current.next;
+            }
+            return false; // data not found
+        }
+    }
+}
+   
