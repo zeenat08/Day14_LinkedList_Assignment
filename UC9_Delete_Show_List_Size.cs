772ee5b76c5e2d68f2ec01ff8676184d9b0f0d04
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:59:56 +0530

    ZEENAT: deleted value and showing list size

---
+++ UC9_Delete_Show_List_Size.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_LinkedList_Assignments
+{
+    class UC9_Delete_Show_List_Size
+    { 
+
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal bool Search(Node head, int x)
+        {
+            Node current = head; // Initialize current
+            while (current != null)
+            {
+                if (current.data == x)
+                    return true; // data found
+                current = current.next;
+            }
+            return false; // data not found
+        }
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) //
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+
+
+    }
+}
