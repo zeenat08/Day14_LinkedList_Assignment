cf81ce44afc7f26d82c65d5a2dc79e632b8ab0b9
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:54:24 +0530

    ZEENAT: inserted value to the list

---
+++ UC3_Appending.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_LinkedList_Assignments
+{
+    class UC3_Appending
+    {
+    }
+}
---
+++ UC4_Insert.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_LinkedList_Assignments
+{
+    class UC4_Insert
+    {
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        internal Node Insert(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) //
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+    }
+}
