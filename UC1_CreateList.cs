b28787a18aa6b0a7c1b8de7a5c1daef2ebb61c9c
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:50:30 +0530

    ZEENAT: added node class

---
+++ UC1_CreateList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_LinkedList_Assignments
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
+
