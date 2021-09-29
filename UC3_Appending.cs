34ea39624a79a55406427fb12e7a4baed6d8d402
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-30 00:05:20 +0530

    ZEENAT: added appending of value to link list

--- UC3_Appending.cs
+++ UC3_Appending.cs
@@ -6,5 +6,57 @@ namespace Day14_LinkedList_Assignments
 {
     class UC3_Appending
     {
+         
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        public void append(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (head == null)
+            {
+                head = new Node(new_data);
+                return;
+            }
+            new_node.next = null;
+            Node last = head;
+            while (last.next != null)
+                last = last.next;
+            last.next = new_node;
+            Console.WriteLine(new_data + " append to previous node");
+            return;
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
     }
 }
+
+    
+
