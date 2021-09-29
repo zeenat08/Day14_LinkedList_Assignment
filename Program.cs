a1847a77ac6e2755f3e5db563fa675d51e0cc608
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-30 00:14:00 +0530

    ZEENAT: added main method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace Day14_LinkedList_Assignments
     {
         static void Main(string[] args)
         {
-
             #region UC2_AddToList
 
             UC2_AddToList list = new UC2_AddToList();
@@ -19,6 +18,12 @@ namespace Day14_LinkedList_Assignments
 
 
             #region UC3_Appending
+
+            UC3_Appending list3 = new UC3_Appending();
+            list3.Add(56);
+            list3.append(30);
+            list3.append(70);
+            list3.Display();
 
             #endregion
 
@@ -71,7 +76,7 @@ namespace Day14_LinkedList_Assignments
             list7.Add(30);
             list7.Add(70);
             if (list7.Search(list7.head, 30))
-                Console.WriteLine("Yes,Serached Value Found");
+                Console.WriteLine("Yes,Searched Value(30) Found");
             else
                 Console.WriteLine("Searched Value Not Found");
 
@@ -81,12 +86,21 @@ namespace Day14_LinkedList_Assignments
 
             #region UC8_InsertAtParticularPosition
 
-            UC8_InsertAtParticularPosition list0 = new UC8_InsertAtParticularPosition();
-            list0.Add(56);
-            list0.Add(30);
-            list0.Add(70);
-            list0.InsertAtParticularPosition(3, 40);
-            list0.Display();
+            UC8_InsertAtParticularPosition list8 = new UC8_InsertAtParticularPosition();
+            list8.Add(56);
+            list8.Add(30);
+            list8.Display();
+            if (list8.Search(list8.head, 30))
+            {
+                Console.WriteLine("\nYes,Serached Value(30) Found");
+                list8.InsertAfter(list8.head.next, 40);
+                Console.WriteLine("\n40 Inserted After Searched Value(30)");
+                list8.Display();
+            }
+            else
+                Console.WriteLine("Searched Value Not Found");
+            list8.Add(70);
+            list8.Display();
 
             #endregion
 
@@ -97,16 +111,47 @@ namespace Day14_LinkedList_Assignments
             UC9_Delete_Show_List_Size list9 = new UC9_Delete_Show_List_Size();
             list9.Add(56);
             list9.Add(30);
-            list9.Add(70);
-            list9.InsertAtParticularPosition(2, 40);
-            if (list9.Search(list9.head, 40))
-                Console.WriteLine("Yes,Serached Value Found");
+            list9.Display();
+            if (list9.Search(list9.head, 30))
+            {
+                Console.WriteLine("\nYes,Searched Value(30) Found");
+                list9.InsertAfter(list9.head.next, 40);
+                Console.WriteLine("\n40 Inserted After Searched Value(30)");
+                list9.Display();
+            }
             else
+            {
                 Console.WriteLine("Searched Value Not Found");
+            }
+
+            list9.Add(70);
+            list9.Display();
+            list9.DeleteNode(40);
+            Console.WriteLine("\n40 Again Removed from List");
+            list9.Display();
+            Console.WriteLine("\nAfter Deleting the node the List Size is: " + list9.ShowListSize());
 
             #endregion
-        }
 
+
+
+            //#region
+
+            //    //UC10_AscendingList list10 = new UC10_AscendingList();
+            //    //list10.Add(56);
+            //    //list10.Add(30);
+            //    //list10.Add(40);
+            //    //list10.Add(70);
+            //    //Console.WriteLine("Linked List before Sorting..");
+            //    //list10.Display();
+            //    Console.WriteLine("\nLinkedList After sorting");
+            //    list10.Display();
+
+            //    #endregion
+            //}
+        }
     }
 }
+
+
 
